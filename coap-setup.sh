e41b97113df4ded487238a327abfeed83eb9be65
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-13 10:32:02 +0100

    Update coap-setup.sh

--- coap-setup.sh
+++ coap-setup.sh
@@ -2,6 +2,8 @@
 
 # Check for root 
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+REALUSER=${SUDO_USER}
+[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
 # Install missing packages
 echo "Installing misc. packages"
