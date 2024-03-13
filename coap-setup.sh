5407cd302ffe532aadf6c7b4217e48b89e702d3a
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-13 10:04:35 +0100

    Update coap-setup.sh

--- coap-setup.sh
+++ coap-setup.sh
@@ -31,6 +31,15 @@ else
   sudo -u ${REALUSER} xrdb -merge $XRESOURCES
 fi
 
+# HACK GLOXBDITWPFR
+grep -q -e "GLOXBDITWPFR" /root/.bashrc
+if [ $? -ne 0 ]; then
+  echo '# HACK GLOXBDITWPFR' >> /root/.bashrc
+  echo 'if [ ! -z "$(ip netns identify)" ]; then' >> /root/.bashrc
+  echo '  export PS1="$(ip netns identify):\w# "' >> /root/.bashrc
+  echo 'fi' >> /root/.bashrc
+fi
+
 # Fix
 grep -q -e "^export LD_LIBRARY_PATH=" /root/.bashrc
 if [ $? -ne 0 ]; then
