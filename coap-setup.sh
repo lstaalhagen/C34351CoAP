a76b7367563dfc8924e3b1645d887066b79620fa
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-13 10:03:47 +0100

    Update coap-setup.sh

--- coap-setup.sh
+++ coap-setup.sh
@@ -16,13 +16,25 @@ make
 make install
 cd ..
 
+# Hack to get a better default font size in Xterm windows
+XRESOURCES=/home/${REALUSER}/.Xresources
+if [ -s $XRESOURCES ]; then
+  grep -q -e "^xterm\*faceName:" $XRESOURCES
+  if [ $? -ne 0 ]; then
+    echo "xterm*faceName: Monospace" >> $XRESOURCES
+    echo "xterm*faceSize: 14" >> $XRESOURCES
+    sudo -u ${REALUSER} xrdb -merge $XRESOURCES
+  fi
+else
+  echo "xterm*faceName: Monospace" > $XRESOURCES
+  echo "xterm*faceSize: 14" >> $XRESOURCES
+  sudo -u ${REALUSER} xrdb -merge $XRESOURCES
+fi
+
 # Fix
 grep -q -e "^export LD_LIBRARY_PATH=" /root/.bashrc
 if [ $? -ne 0 ]; then
   echo "export LD_LIBRARY_PATH=/usr/local/lib" >> /root/.bashrc
 fi
 
-# Maybe???
-echo "export LD_LIBRARY_PATH=\"/usr/local/lib\"" >> /home/user/.profile
-
-# cd /home/user/C34351CoAP
+echo -e "\n\n *** Please restart the Virtual Machine *** "
